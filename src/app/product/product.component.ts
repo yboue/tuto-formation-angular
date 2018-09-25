166685e53019b67b8a26b471af89130bfaaf5619
Herminaël ROUGIER <herminael.rougier@zenika.com>
2018-09-25 21:30:49 +0200

    tp3 - composants

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  @Output()
+  addToBasket = new EventEmitter<Product>();
+
+  @Input()
+  data: Product;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  addToBasketClick() {
+    this.addToBasket.emit(this.data);
+  }
+
+}
